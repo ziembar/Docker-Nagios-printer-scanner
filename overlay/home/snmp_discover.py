9e4d1cc7fd1a0ba02a5509af1c496774eb616a18
Is0meone <115733083+Is0meone@users.noreply.github.com>
2024-05-29 22:52:57 +0200

    Multiple SNMP

--- overlay/home/snmp_discover.py
+++ overlay/home/snmp_discover.py
@@ -16,7 +16,6 @@ def discover_hosts():
     else:
         delete_existing_configs(output_dir)
 
-    # Retrieve the network from the environment variable
     network = os.getenv('NETWORK')
     if not network:
         raise ValueError("NETWORK environment variable is not set")
@@ -26,7 +25,6 @@ def discover_hosts():
     except ValueError as e:
         raise ValueError(f"Invalid network format: {e}")
 
-    # Initialize the nmap scanner
     nm = nmap.PortScanner()
     print("Scanning network for active hosts (ICMP)...")
     nm.scan(hosts=str(network), arguments='-sn --unprivileged')
@@ -41,9 +39,11 @@ def discover_hosts():
 
     host_names = {}
 
+    hostgroup_defined = False
+
     for ip in active_hosts:
-        community = 'public'    # Change this to your SNMP community string
-        snmp_timeout = 0.05     # Timeout in seconds
+        community = 'public'
+        snmp_timeout = 0.05
 
         error_indication, error_status, error_index, var_binds = next(
             getCmd(SnmpEngine(),
@@ -71,32 +71,26 @@ def discover_hosts():
 
                 print(f"SNMP! Saved config file for ip: {ip}")
                 with open(f"{output_dir}{unique_name}.cfg", "w") as f:
-                    f.write(f"""
+                    config_content = f"""
 define host {{
-    use                     generic-printer         ; Inherit default values from a template
-    host_name               {unique_name}           ; The name we're giving to this printer
-    alias                   {full_name}             ; A longer name associated with the printer
-    address                 {ip}                    ; IP address of the printer
-    hostgroups              network-printers        ; Host groups this printer is associated with
-}}
-
-define hostgroup {{
-    hostgroup_name          network-printers        ; The name of the hostgroup
-    alias                   Network Printers        ; Long name of the group
+    use                     generic-printer
+    host_name               {unique_name}
+    alias                   {full_name}
+    address                 {ip}
+    hostgroups              network-printers
+    max_check_attempts      3
 }}
 
 define service {{
-
-    use                     generic-service         ; Inherit values from a template
-    host_name               {unique_name}              ; The name of the host the service is associated with
-    service_description     Printer Status          ; The service description
-    check_command           check_hpjd!-C public    ; The command used to monitor the service
-    check_interval          10                      ; Check the service every 10 minutes under normal conditions
-    retry_interval          1                       ; Re-check the service every minute until its final/hard state is determined
+    use                     generic-service
+    host_name               {unique_name}
+    service_description     Printer Status
+    check_command           check_hpjd!-C public
+    check_interval          10
+    retry_interval          1
 }}
 
 define service {{
-
     use                     generic-service
     host_name               {unique_name}
     service_description     PING
@@ -105,18 +99,27 @@ define service {{
     retry_interval          1
 }}
 
-
 define service{{
-       use                             generic-service
-       host_name               {unique_name}
-       hostgroup_name                  network-printers
-       service_description             printer status
-       check_command                   check_netsnmp2! -H {ip}
-       notification_options            c,u,r
-       check_interval          1
+    use                     generic-service
+    host_name               {unique_name}
+    hostgroup_name          network-printers
+    service_description     printer status
+    check_command           check_netsnmp2! -H {ip}
+    notification_options    c,u,r
+    check_interval          1
+}}
+                    """
+                    f.write(config_content)
 
-       }}
-                    """)
+                if not hostgroup_defined:
+                    with open(f"{output_dir}network_printers.cfg", "w") as fg:
+                        fg.write("""
+define hostgroup {
+    hostgroup_name          network-printers
+    alias                   Network Printers
+}
+                        """)
+                    hostgroup_defined = True
 
 if __name__ == "__main__":
     discover_hosts()
